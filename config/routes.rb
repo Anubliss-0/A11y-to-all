9de41d1fc7fe9c1386450624b2bbfb26b0800c6a
Thomsd Rice <tom@anubliss.com>
2022-08-29 14:32:56 +0200

    Project created

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :profiles, only: [:create, :show, :update]
+  resources :tools
+  resources :lists, only: [:create, :destroy, :show]
+  resources :bookmarks, only: [:create, :destroy]
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
