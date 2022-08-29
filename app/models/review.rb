47bb1493608f0c5319db3a0ab5d140cabc9347d7
Clayton Flaherty <claytonflaherty92@gmail.com>
2022-08-29 15:40:14 +0200

    added colons

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
-  belongs_to profile
-  belongs_to tool
-  belongs_to review
+  belongs_to :profile
+  belongs_to :tool
+  belongs_to :review
   validate :profile_id, :resource_id, :text, :rating, presence: true
 end
--- app/models/tool.rb
+++ app/models/tool.rb
@@ -1,7 +1,7 @@
 class Tool < ApplicationRecord
-  belongs_to user
-  has_many bookmarks
-  has_many resource_categories
-  has_many reviews
+  belongs_to :profile
+  has_many :bookmarks
+  has_many :resource_categories
+  has_many :reviews
   valiates :title, :descripation, :url, :user_id, presence: true
 end
