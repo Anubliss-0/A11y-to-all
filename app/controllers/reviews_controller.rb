f385b6f0cae4038147b10f03023ef3cb5e8affbd
Thomsd Rice <tom@anubliss.com>
2022-08-30 17:30:29 +0200

    started building review functionality

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,11 @@
 class ReviewsController < ApplicationController
+  before_action :set_review, only: %i[show edit update destroy]
+
+
+
+  private
+
+  def set_tool
+    @tool = Tool.find(params[:id])
+  end
 end
