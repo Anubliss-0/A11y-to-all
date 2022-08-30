12c7edf9d92f45a47d25b8a636b4aa058840eb41
Thomsd Rice <tom@anubliss.com>
2022-08-30 13:08:27 +0200

    Completed index with pundit functionality

---
+++ app/policies/tool_policy.rb
@@ -0,0 +1,32 @@
+class ToolPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    def resolve
+      scope.all
+    end
+  end
+
+  def show?
+    return true
+  end
+
+  def new?
+    return create?
+  end
+
+  def create?
+    return true
+  end
+
+  def edit?
+    return update?
+  end
+
+  def update?
+    record.user == user
+  end
+
+  def destroy?
+    record.user == user
+  end
+end
