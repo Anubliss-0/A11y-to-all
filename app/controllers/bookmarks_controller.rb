575067e3ddb28b0514cd411647b73f81041bab9f
Clayton Flaherty <claytonflaherty92@gmail.com>
2022-09-01 17:47:40 +0200

    set up form, form will not display if user has no lists

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,6 +1,9 @@
 class BookmarksController < ApplicationController
   def new
-
+    @bookmark = Bookmark.new
+    authorize @bookmark
+    @lists = List.select { |list| list.user_id == current_user.id }
+    @tool = Tool.find(params[:tool_id])
   end
 
   def create
