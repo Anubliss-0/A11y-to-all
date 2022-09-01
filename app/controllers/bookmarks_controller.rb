52ec2b47c53b2ebcab598a3cc1555bed1ffbe456
Clayton Flaherty <claytonflaherty92@gmail.com>
2022-09-01 17:56:34 +0200

    code written but bookmarks not saving

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -7,6 +7,21 @@ class BookmarksController < ApplicationController
   end
 
   def create
+    params[:bookmark][:list_id].delete("")
+    params[:bookmark][:list_id].each do |list|
+      @bookmark = Bookmark.new(bookmark_params)
+      authorize @bookmark
+      @bookmark.list_id = list
+      @tool = Tool.find(params[:tool_id])
+      @bookmark.tool_id = @tool
+      @bookmark.save
+    end
+    redirect_to profile_path(current_user.profile)
+  end
 
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:list_id)
   end
 end
