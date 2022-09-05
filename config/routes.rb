4d578b69e582e7edcd2d6160aead62b21fd0fa93
Clayton Flaherty <claytonflaherty92@gmail.com>
2022-09-05 09:58:30 +0200

    message routes, chatroom basic stylesheet imported

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,9 @@ Rails.application.routes.draw do
   resources :reviews, only: [:destroy, :update, :edit]
   resources :lists
   resources :profiles
-  resources :chatrooms, only: :show
+  resources :chatrooms, only: :show do
+    resources :messages, only: :create
+  end
 
   get "test", to: "pages#test"
   get "dashboard", to: "dashboard#reccomend"
